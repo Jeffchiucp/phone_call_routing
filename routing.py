ef7173d05e784d34c449b4006878d4cb0cba3b78
Jeffchiucp <jeffchiu2000@gmail.com>
2017-11-03 15:54:02 -0700

    added codes and phone number

--- routing.py
+++ routing.py
@@ -1,7 +1,20 @@
-def route_cost_check(filename):
-    data = open(filename, 'r')
+def phone_route_cost_check(filename, number):
+    open_file = open(filename, 'r')
+    data = open_file.readlines()
+
+    number_cost = []
     for line in data:
         print(line)
-    print(data)
+        for i in range(0, len(line)):
+            if line[i] == ',':
+                print(i)
+                number = line[0:i]
+                price = line[i+1:-1]
+                number_cost.append((number, price))
 
-route_cost_check("phone-numbers-10000.txt")
+    print( number_cost)
+    open_file.close()
+
+# print out the number of the phone numbers
+
+phone_route_cost_check("phone-numbers-100.txt", '+14105547746')

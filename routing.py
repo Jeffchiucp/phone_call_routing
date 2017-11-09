4f1b289346ab5ec970bcd74f9f467814475459f4
Jeffchiucp <jeffchiu2000@gmail.com>
2017-11-08 17:03:17 -0800

    updated solution grep '+1412340' route-costs-106000.txt'

--- routing.py
+++ routing.py
@@ -4,20 +4,18 @@ def phone_route_cost_check(filename, number):
 
     number_cost = []
     for line in data:
-        print(line)
         for i in range(0, len(line)):
             if line[i] == ',':
                 print(i)
                 number = line[0:i]
                 price = line[i+1:-1]
                 number_cost.append((number, price))
-
-    print( number_cost)
+    # print( number_cost)
     open_file.close()
 
 # print out the number of the phone numbers
 
-phone_route_cost_check("route-costs-100.txt", '+14105547746')
+phone_route_cost_check("route-costs-106000.txt", '+1412340')
 # routefile = open("route-costs-106000.txt").read().split("\n")
 # phonenum = "+861721532"
 # print("Looking for", phonenum)
